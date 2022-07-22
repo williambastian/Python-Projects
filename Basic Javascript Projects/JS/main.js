577962b8b9d544d4748fce74c19c9fb4bc54fc3b
William <87213388+williambastian@users.noreply.github.com>
2022-07-22 16:59:49 -0500

    updated main.js

--- Basic Javascript Projects/JS/main.js
+++ Basic Javascript Projects/JS/main.js
@@ -3,15 +3,15 @@ window.alert("Hello World!");
 //print hello again on page//
 document.write("Hello Again!");
 //assigning and concatenating string variables//
-var Sent1 = " This is the beginning of a string.";
-//document.write(A);//
+var Sent1 = " This is the beginning of a string,";
+
 var Sent2 = " and this is the end of the string.";
 document.write(Sent1+Sent2);
-//window.alert(B);//
-var C = "\"This is a quote within a string, isn't it?\""
+
+var C = " \"This is a quote within a string, isn't it?\""
 document.write(C);
 //var D is a string concatenation and an expression//
-var D = "String 1 plus "+"string 2 "+"equals string 3.";
+var D = " String 1 plus "+"string 2 "+"equals string 3. ";
 document.write(D);
 var fam = "The Smiths", dad = "John", mom ="Jaye", son = "Cobb", daughter = "Jingleheimer";
 //document.write is a statement//
