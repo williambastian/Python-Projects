9566661b6eacbe3606d0531a682fd7736a401836
William <87213388+williambastian@users.noreply.github.com>
2022-07-22 16:31:17 -0500

    updated JavaScript project files

---
+++ Basic Javascript Projects/JS/main.js
@@ -0,0 +1,18 @@
+//window popup that says hello world//
+window.alert("Hello World!");
+//print hello again on page//
+document.write("Hello Again!");
+//assigning and concatenating string variables//
+var A = " This is a string.";
+document.write(A);
+var B = "This is another string.";
+window.alert(B);
+var C = "\"This is a quote within a string, isn't it?\""
+document.write(C);
+//var D is a string concatenation and an expression//
+var D = "String 1 plus "+"string 2 "+"equals string 3.";
+document.write(D);
+var fam = "The Smiths", dad = "John", mom ="Jaye", son = "Cobb", daughter = "Jingleheimer";
+//document.write is a statement//
+document.write(daughter);
+
